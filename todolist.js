a6d833ed81d4209ac46b039c96da820f78e659d4
ram071985 <reidmuchow@gmail.com>
2019-12-10 15:33:18 -0600

    added li creator

--- todolist.js
+++ todolist.js
@@ -5,14 +5,18 @@ let toDolist = [];
 
 document.getElementById('js-todo-input').addEventListener('keydown', function(evt) {
     evt.preventDefault();
-    if (evt.code === 'Enter' ); {
-        console.log('Enter KEy');
-        document.getElementById('todo-item').innerHTML = text;
+    if (evt.code === 'Enter' ) {
+        console.log('Enter key was Pressed');
+
+        
+        document.getElementById('js-todo-input').innerHTML;
         
     }
 });
 
-function makeUL(array) {
-    let list = document.createElement('ul');
+function makeList(array) {
+    let newEl = document.createElement('li');
+    let newText = document.createTextNode('');
+    newEl.appendChild(newText);
 }
 let text = document.getElementById('js-todo-input').innerHTML;
