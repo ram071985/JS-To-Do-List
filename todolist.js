710fc1904c10f590cfd9789e5ccaeaa437070172
ram071985 <reidmuchow@gmail.com>
2019-12-11 16:28:18 -0600

    added <li> createTextNode

--- todolist.js
+++ todolist.js
@@ -1,15 +1,21 @@
 /* Reid Muchow'a To Do List App */
 let toDolist = [];
-
+let grabText = document.getElementById('js-todo-input').innerHTML;
+let newEl = document.createElement('li');
+let newText = document.createTextNode(grabText);
 
 
 document.getElementById('js-todo-input').addEventListener('keydown', function(evt) {
     if (evt.code === 'Enter' ) {
+       
         evt.preventDefault();
-        console.log('Enter');
+        console.log('enter')
         document.getElementById('js-todo-input').innerHTML;
-        
+        newEl.appendChild(newText)
+
     }
 });
+
+
 
 
