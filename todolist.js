511ad4172999e9086d383f305f3cf8bd25560e36
ram071985 <reidmuchow@gmail.com>
2019-12-10 18:22:09 -0600

    stuck on evt.preventDefault

--- todolist.js
+++ todolist.js
@@ -6,17 +6,9 @@ let toDolist = [];
 document.getElementById('js-todo-input').addEventListener('keydown', function(evt) {
     evt.preventDefault();
     if (evt.code === 'Enter' ) {
-        console.log('Enter key was Pressed');
-
-        
         document.getElementById('js-todo-input').innerHTML;
         
     }
 });
 
-function makeList(array) {
-    let newEl = document.createElement('li');
-    let newText = document.createTextNode('');
-    newEl.appendChild(newText);
-}
-let text = document.getElementById('js-todo-input').innerHTML;
+
