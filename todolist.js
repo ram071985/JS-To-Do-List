8d19a3c40ec0271bbdf5a0072f2841f72a6679da
ram071985 <reidmuchow@gmail.com>
2019-12-13 15:43:40 -0600

    added click event listener

--- todolist.js
+++ todolist.js
@@ -7,21 +7,32 @@
 document.getElementById('js-todo-input').addEventListener('keydown', function(evt) {
     if (evt.code === 'Enter' ) {
         evt.preventDefault();
-       
         let newEl = document.createElement('li');
         let text = evt.target.value;
         let newText = document.createTextNode(text);
+        newEl.innerHTML = `<button>delete</button>`;
         newEl.appendChild(newText);
         let position = document.getElementsByTagName('ul')[0];
         position.appendChild(newEl);
-        evt.target.value = ' ';
+        evt.target.value = '';
         
 
-
-
     }
+
+    
+        
+    
 });
 
+documents.getElementsByTagName('ul').addEventListener('click','button', function(e) {
+    e.preventDefault();
+    let removeEl = document.getElementsByTagName('li');
+    let containerEl = removeEl.parentNode;
+    containerEl.removeChild(removeEl);
+
+}
 
 
- 
+
+});
+
