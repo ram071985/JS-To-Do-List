73dd0effcd82c9d34d4be1b8e43f203f370353a6
ram071985 <reidmuchow@gmail.com>
2019-12-13 12:06:42 -0600

    added blank text to input box

--- todolist.js
+++ todolist.js
@@ -3,18 +3,25 @@
 
 
 
+
 document.getElementById('js-todo-input').addEventListener('keydown', function(evt) {
     if (evt.code === 'Enter' ) {
         evt.preventDefault();
+       
         let newEl = document.createElement('li');
         let text = evt.target.value;
         let newText = document.createTextNode(text);
         newEl.appendChild(newText);
         let position = document.getElementsByTagName('ul')[0];
         position.appendChild(newEl);
+        evt.target.value = ' ';
+        
+
+
+
     }
 });
 
 
 
-
+ 
