5281b3298857c0c80cde400720e5a86837d3cda8
ram071985 <reidmuchow@gmail.com>
2019-12-14 18:26:09 -0600

    added click event and bdelete button

--- todolist.js
+++ todolist.js
@@ -7,32 +7,49 @@
 document.getElementById('js-todo-input').addEventListener('keydown', function(evt) {
     if (evt.code === 'Enter' ) {
         evt.preventDefault();
+
+        /* List Creator */
         let newEl = document.createElement('li');
         let text = evt.target.value;
         let newText = document.createTextNode(text);
-        newEl.innerHTML = `<button>delete</button>`;
         newEl.appendChild(newText);
         let position = document.getElementsByTagName('ul')[0];
         position.appendChild(newEl);
-        evt.target.value = '';
-        
 
+        /* Button Creator */
+        let deleteButton = document.createElement('button');
+        let deleteText = 'Delete';
+        let buttonText = document.createTextNode(deleteText);
+        deleteButton.appendChild(buttonText);
+        let posDelete = document.getElementsByTagName('ul')[0];
+        posDelete.appendChild(newEl);
+
+        /* form submission value */
+        evt.target.value = '';
+
+        /* Delete Button Click Functionality */
+        deleteButton.addEventListener('click', function(del) {
+          let removeEl = document.getElementsByTagName('li')[0];
+          let containerEl = removeEl.parentNode;
+          containerEl.removeChild(removeEl);
+
+
+        })
+        
     }
 
+ 
+    
+
+  })
+
+
     
         
     
-});
-
-documents.getElementsByTagName('ul').addEventListener('click','button', function(e) {
-    e.preventDefault();
-    let removeEl = document.getElementsByTagName('li');
-    let containerEl = removeEl.parentNode;
-    containerEl.removeChild(removeEl);
-
-}
 
 
 
-});
+
+
 
