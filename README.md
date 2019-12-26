1b0fb853ca52555d01ed015f86822f51eb173c93
ram071985 <reidmuchow@gmail.com>
2019-12-26 13:26:58 -0600

    cleaned code, fixed image in readme.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 This to do list is a fast and conveinant way to keep track of your day to day tasks.  You can cross items off when you have completed them and delete each item seperately.  For a live running version go to my GitHub Page  - [HERE](https://ram071985.github.io/to-do-list/)
 
 [Codepen](https://codepen.io/reid-muchow/pen/mdyOprP)
-![GitHub](https://github.com/ram071985/to-do-list)
+
+![GitHub](to-do-list.png)
 )
 ## Summary
 
--- todolist.js
+++ todolist.js
@@ -1,57 +1,65 @@
 /* Reid Muchow'a To Do List App */
 
+function createLiElement(toDoItemText) {
+  const liElement = document.createElement("li");
+  const liTextNode = document.createTextNode(toDoItemText);
+  liElement.appendChild(liTextNode);
+
+  /* Click Strikethrough */
+  liElement.addEventListener("click", function(e) {
+    let strike = (e.target.style.textDecoration = "line-through");
+    if (liElement.value.length > 0) {
+      return strike;
+    }
+  });
+
+  return liElement;
+}
+
+function createDeleteButton() {
+  const deleteButton = document.createElement("button");
+  const deleteText = "Delete";
+  const buttonText = document.createTextNode(deleteText);
+  deleteButton.appendChild(buttonText);
+
+  return deleteButton;
+}
+
 document
   .getElementById("js-todo-input")
   .addEventListener("keydown", function(evt) {
     if (evt.code === "Enter") {
       evt.preventDefault();
 
+      let ulElement = document.getElementById("todo-list");
+
       /* List Creator */
-      let newEl = document.createElement("li");
-      let text = evt.target.value;
-      let newText = document.createTextNode(text);
-      newEl.appendChild(newText);
-      let position = document.getElementsByTagName("ul")[0];
-      position.appendChild(newEl);
+      let liElement = createLiElement(evt.target.value);
+      ulElement.appendChild(liElement);
 
       /* Button Creator */
-      let deleteButton = document.createElement("button");
-      let deleteText = "Delete";
-      let buttonText = document.createTextNode(deleteText);
-      deleteButton.appendChild(buttonText);
-      let posDelete = document.getElementsByTagName("ul")[0];
-      posDelete.appendChild(deleteButton);
-
-      /* form submission value */
-      evt.target.value = "";
+      let deleteButton = createDeleteButton();
 
       /* Delete Button Click Functionality */
       deleteButton.addEventListener("click", function() {
-        let removeEl = document.getElementsByTagName("li")[0];
-        let containerEl = removeEl.parentNode;
-        containerEl.removeChild(removeEl);
+        let removeEl = e.target;
+        let liEl = removeEl.parentNode;
+        ulElement.removeChild(liEl);
       });
+      ulElement.appendChild(deleteButton);
 
-      /* Delete Button Delete Funtionality*/
-
-      deleteButton.addEventListener("click", function() {
-        let removeButton = document.getElementsByTagName("button")[0];
-        let containerButton = removeButton.parentNode;
-        containerButton.removeChild(removeButton);
-      });
-
-      /* Click Strikethrough */
-      newEl.addEventListener("click", function(s) {
-        let strike = (s.target.style.textDecoration = "line-through");
-        if (newEl.value.length > 0) {
-          return strike;
-        }
-      });
-      
-      window.localStorage.setItem('list', JSON.stringify(newText));
-      JSON.parse(window.localStorage.getItem('list'));
-      
+      /* form submission value */
+      evt.target.value = "";
+// store new todo item in local storage
+// check to see if there is any new exiting array in local storage
+// if not, create a new array and add an item to it
+// if so, add new item to array
+// update local storage with new array 
+      window.localStorage.setItem("list", JSON.stringify(evt.target.value));
+      let returnValue = localStorage.getItem("list");
     }
-
-    
   });
+
+//retrieve the to do list from local storage
+//should have array in local storage
+//loop through each item in array, and display items on page
